1f7479a7adc4d589f075f6db59600e6c374d0224
radovanbotik <radovanbotik@gmail.com>
2022-09-07 18:45:04 +0200

    crypto feature

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const authorName = document.querySelector("#authorName");
+const cryptoImage = document.querySelector(".crypto-image");
+const cryptoName = document.querySelector(".crypto-name");
 
 const imageURL =
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=donut";
@@ -13,7 +15,7 @@ const fetchCrypto = async () => {
   const resp = await fetch(cryptoURL);
   if (resp.ok) {
     const data = await resp.json();
-    console.log(data);
+    console.log(data.name, data.image.small);
     return data;
   } else {
     throw new Error("error while fetching from gecko");
@@ -23,9 +25,11 @@ const fetchCrypto = async () => {
 const updateDOM = async () => {
   try {
     const imageData = await fetchBackground();
-    const crypToData = await fetchCrypto();
+    const cryptoData = await fetchCrypto();
     document.body.style.backgroundImage = `url(${imageData.urls.regular})`;
     authorName.textContent = `Image credit: ${imageData.user.name}`;
+    cryptoImage.src = cryptoData.image.small;
+    cryptoName.textContent = cryptoData.name;
   } catch (error) {
     document.body.style.backgroundImage =
       'url("https://images.unsplash.com/photo-1593445203635-af51d171f25e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI1NjA0NTg&ixlib=rb-1.2.1&q=80&w=1080")';
