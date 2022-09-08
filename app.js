d30ee48c94ab12acfb888baf0c0a6ed8db6ff0cd
radovanbotik <radovanbotik@gmail.com>
2022-09-08 05:10:35 +0200

    using geolocation API

--- app.js
+++ app.js
@@ -18,19 +18,25 @@ const fetchCrypto = async () => {
   const resp = await fetch(cryptoURL);
   if (resp.ok) {
     const data = await resp.json();
-    console.log(data.market_data);
     return data;
   } else {
     throw new Error("error while fetching from gecko");
   }
 };
 
-const getTime = () => {
+const renderTime = () => {
   const newDate = new Date();
   const currentTime = newDate.toLocaleTimeString("sk-SK");
-  return currentTime;
+  timeBody.textContent = currentTime;
 };
 
+const getLocation = () => {
+  navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude);
+  });
+};
+getLocation();
 const updateDOM = async () => {
   try {
     const imageData = await fetchBackground();
@@ -50,9 +56,7 @@ const updateDOM = async () => {
     <p>price low : ${cryptoData.market_data.low_24h.eur}</p>
     `;
     //update Time DOM
-    setInterval(() => {
-      timeBody.textContent = getTime();
-    }, 1000);
+    setInterval(renderTime, 1000);
   } catch (error) {
     document.body.style.backgroundImage =
       'url("https://images.unsplash.com/photo-1593445203635-af51d171f25e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI1NjA0NTg&ixlib=rb-1.2.1&q=80&w=1080")';
