fa45b8921017cefe8c1325509e0b68657419625a
radovanbotik <radovanbotik@gmail.com>
2022-09-08 13:20:01 +0200

    using geolocation api, getting my location, facing issue with return, solved by warpping function in promise

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const cryptoMarket = document.querySelector(".crypto-market");
 const imageURL =
   "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=donut";
 const cryptoURL = "https://api.coingecko.com/api/v3/coins/bitcoin";
+const weatherURL = "https://apis.scrimba.com/openweathermap/data/2.5/weather";
 
 const fetchBackground = async () => {
   const resp = await fetch(imageURL);
   const data = await resp.json();
   return data;
 };
-const fetchCrypto = async () => {
+const getCrypto = async () => {
   const resp = await fetch(cryptoURL);
   if (resp.ok) {
     const data = await resp.json();
@@ -24,23 +25,33 @@ const fetchCrypto = async () => {
   }
 };
 
-const renderTime = () => {
+const getTime = () => {
   const newDate = new Date();
   const currentTime = newDate.toLocaleTimeString("sk-SK");
   timeBody.textContent = currentTime;
 };
 
+// const getLocation = navigator.geolocation.getCurrentPosition(position => {
+//   const { latitude, longitude } = position.coords;
+//   return position.coords;
+// });
+
 const getLocation = () => {
-  navigator.geolocation.getCurrentPosition(position => {
-    const { latitude, longitude } = position.coords;
-    console.log(latitude, longitude);
+  return new Promise((resolved, rejected) => {
+    navigator.geolocation.getCurrentPosition(resolved);
   });
 };
-getLocation();
+const getCoords = async () => {
+  const location = await getLocation();
+  const { latitude, longitude } = location.coords;
+  return { latitude, longitude };
+};
+
+getCoords();
 const updateDOM = async () => {
   try {
     const imageData = await fetchBackground();
-    const cryptoData = await fetchCrypto();
+    const cryptoData = await getCrypto();
     //handling background image
     document.body.style.backgroundImage = `url(${imageData.urls.regular})`;
     authorName.textContent = `Image credit: ${imageData.user.name}`;
@@ -56,7 +67,10 @@ const updateDOM = async () => {
     <p>price low : ${cryptoData.market_data.low_24h.eur}</p>
     `;
     //update Time DOM
-    setInterval(renderTime, 1000);
+    setInterval(getTime, 1000);
+    //handle user location
+
+    // const weather = getWeather(getLocation);
   } catch (error) {
     document.body.style.backgroundImage =
       'url("https://images.unsplash.com/photo-1593445203635-af51d171f25e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjI1NjA0NTg&ixlib=rb-1.2.1&q=80&w=1080")';
